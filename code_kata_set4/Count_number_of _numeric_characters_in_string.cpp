71a5d24f1874e8c963df2d76bd2e639b0cfa87eb
Ranasonuraj <52693912+Ranasonuraj@users.noreply.github.com>
2019-08-07 00:17:22 +0530

    Add files via upload

---
+++ code_kata_set4/Count_number_of _numeric_characters_in_string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string str;
+  int count = 0;
+
+  getline(cin, str);
+
+  for(int ind = 0; str[ind] != '\0'; ind++)
+  {
+      if(str[ind] >= '0' && str[ind] <= '9')
+        count++;
+  }
+
+  cout << count;
+
+  return 0;
+}
---
+++ code_kata_set4/program_to_swap_2_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+int x,y,temp;
+cin>>x>>y;
+temp=x;
+x=y;
+y=temp;
+
+cout<<x<<" "<<y;
+
+}
